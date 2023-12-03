0ee4f08684939600bf1611ba1705e96e5ac61c11
Aleeg0 <Sankafoxe@gmail.com>
2023-12-03 16:41:05 +0300

    InstructionUnit1_1.pas and Project.dpr was  added

---
+++ Laboratory 1_1/Project1.dpr
@@ -0,0 +1,23 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  MainUnit1_1 in 'MainUnit1_1.pas' {MainForm},
+  Vcl.Themes,
+  Vcl.Styles,
+  AboutDeveloperUnit1_1 in 'AboutDeveloperUnit1_1.pas' {AboutTheDeveloper},
+  InstructionUnit1_1 in 'InstructionUnit1_1.pas' {Instruction},
+  ExitUnit1_1 in 'ExitUnit1_1.pas' {Exitunit};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 Dark');
+  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(TAboutTheDeveloper, AboutTheDeveloper1);
+  Application.CreateForm(TInstruction, Instruction1);
+  Application.CreateForm(TExitunit, Exitunit);
+  Application.Run;
+end.
